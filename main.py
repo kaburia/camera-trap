58f634070cb77d4d3d3133426c20f9379cbff079
kaburia <kaburiaaustin1@gmail.com>
2024-04-21 10:17:43 +0300

    point of entry of the system init

---
+++ main.py
@@ -0,0 +1,10 @@
+''''
+Point of entry for camera trap system.
+- Turned on when motion is detected (Done by PIR sensor)
+- Microwave sensor is used to confirm motion
+- Camera is triggered to start recording or capture image if person is detected
+- Video is saved to disk
+- If image captured GSM turned on to send image to the server
+- Video captured by the end of the day is sent to the server and deleted from the disk
+- GSM is turned off
+'''
