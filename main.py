229fec12706ca33d47a07161080d2727a5531bda
kaburia <kaburiaaustin1@gmail.com>
2024-07-25 18:20:22 +0300

    Workflow

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
-''''
-Point of entry for camera trap system.
-- Turned on when motion is detected (Done by PIR sensor)
-- Microwave sensor is used to confirm motion
-- Camera is triggered to start recording or capture image if person is detected
-- Video is saved to disk
-- If image captured GSM turned on to send image to the server
-- Video captured by the end of the day is sent to the server and deleted from the disk
-- GSM is turned off
-'''
+"""
+ Copyright 2024 Dedan Kimathi University of Technology
+ Author: Austin Kaburia (Github: @kaburia)
+ 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from utils.system_config import SetupSystem
+
+# set up the system
+# Set up the real time clock
+SetupSystem.set_rtc((2024, 7, 25, 18, 10, 0, 0, 0))
+
+# Initialize the camera sensor
+SetupSystem.init_camera()
+
+# Infinite loop to check for motion
+while True:
+    # Check if motion is detected
+    if SetupSystem.motion_sensed():
+        # Wake up the camera
+        SetupSystem.wake_up()
+        # Check time
+        if SetupSystem.check_time(): # after 6pm
+            # Transmit the Data
+            
+            # Sleep the system till the next day (for 12 hours)
+            SetupSystem.sleep_mode(timer=True)
+        else: # before 6pm
+            # record the video
+            SetupSystem.record_video()
+    else:
+        # trigger sleep mode
+        SetupSystem.sleep_mode(trigger=True)
